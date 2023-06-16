bf31dbb5b7c56a57a60a52e0ad24fa370166d2b3
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 20:58:52 +0530

    terraform

--- 01. sample.sh
+++
@@ -1,5 +0,0 @@
-ls -l
-pwd
-#yum list installed | grep vi
-echo "welcome to shell script classes"
-echo -e "\e[31mHello in Red color\e[0m"
---
+++ 02.print.sh
@@ -0,0 +1,10 @@
+echo Hello World     # Here echo is used to print the message
+echo -e "\e[COLm colour Text \e[0m"   # -e is used to enable the Colour
+                                      # \e[COLm is used to enable certain colour
+                                      # \e[0m is used to diable the certain colour
+                                      # COL stands for color
+                                      #possible colours are Red(31) Green(32)  Yellow(33)  Blue(34)  Megenta(35) Cyan(36)
+
+echo -e "\e[31mHello in Red color\e[0m"
+
+
