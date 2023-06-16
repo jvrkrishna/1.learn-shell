7f7b9b4a23e416379e9d91f4081580ea9e119c63
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 23:49:26 +0530

    terraform

--- 07.Exit_status.sh
+++ 07.Exit_status.sh
@@ -5,3 +5,35 @@ echo $? ########### to print the status of last executed command true or false  
 
 useradd ravi ############ if we run again adduser with same name then $? shows other than 0
 echo $?
+
+
+Exit
+Sample() {
+	Echo Hello
+	Return 1
+	Echo Bye
+}
+
+Sample
+echo function exit status  - $?
+
+# Echo Hello ------- it will run
+# Exit ------ Here it will exit the shell. Then place Exit 1 instead of Exit because exit it shows exit status 0----- Where there is failure put Exit(1 to 255 any number)
+# Echo bye
+
+Ex:
+status(){
+  if [ $1 -eq 0 ];then
+    echo SUCCESS
+  else
+    echo Failure
+    exit 1
+  fi
+}
+
+If we run the script with normal user the script cannot be run.
+user_id=$(id -u)
+if [ $user_id -ne 0 ]; then
+echo script should be run with sudo
+exit 1
+fi
