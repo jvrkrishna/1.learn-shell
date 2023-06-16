2574db8f57e396e1d7c21a4447c334fa346bb5ae
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 22:28:55 +0530

    terraform

--- 07.Exit_status.sh
+++ 07.Exit_status.sh
@@ -1,3 +1,7 @@
 ############## EXIT STATUS ######################
 useradd ravi ######### uses to add user
-echo $? ########### to print the status of last executed command true or false
+echo $? ########### to print the status of last executed command true or false  #### it shows 0
+
+
+useradd ravi ############ if we run again adduser with same name then $? shows other than 0
+echo $?
