d8dd0a05f5c0c6c82ce59f478cda991aaf0cb303
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 23:41:04 +0530

    terraform

--- 06.functions.sh
+++ 06.functions.sh
@@ -3,3 +3,12 @@ print_hello() {
 }
 
 print_hello
+
+print_arguments() {
+  echo First argument- $1
+  echo Second argument- $2
+  echo All arguments - $*
+  echo No Of Arguments - $#
+}
+
+print_arguments abc 123 xyz
