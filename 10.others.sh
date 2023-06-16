9f70c18c6d9e0ba6f10ee4b584beb0efbd507a3d
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 23:04:39 +0530

    terraform

--- 10.others.sh
+++ 10.others.sh
@@ -6,3 +6,22 @@ status(){
     exit 1
   fi
 }
+
+
+##################Loops
+##################While loop
+a=10
+while [ $a -gt 0 ]; do
+echo Hello World
+a=$(($a-1))
+done
+
+####################for loop
+for component in catalogue user cart frontend shipping payment; do
+echo creating component -  $component
+sleep 1
+done
+
+
+
+
