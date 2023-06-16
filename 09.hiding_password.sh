a8ac2d3ce38ac1222c804fd7bb4968ec0998fe00
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 22:47:51 +0530

    terraform

--- 09.hiding_password.sh
+++ 09.hiding_password.sh
@@ -1,2 +1,15 @@
 App_password=$1   #####through password in execution sudo bash 09.hiding_password.sh Apple
 echo ${App_password}
+
+########## Example #################
+#For password hiding
+# in Payment.sh
+# App_password=$1
+
+#in Payment.service
+#Environment=AMQP_PASS=app_password
+
+#Common.sh----- in python
+#cp /home/centos/roboshop-shell/${component}.service  /etc/systemd/system/${component}.service &>>${logfile}
+#sed -i "s/app_password/$app_password/" /etc/systemd/system/${component}.service &>>${logfile}
+
