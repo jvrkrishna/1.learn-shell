cb4fa33b148b7ca7e910c0daa39f5af4c75a1f62
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 22:01:38 +0530

    terraform

--- 05.variables.sh
+++ 05.variables.sh
@@ -7,3 +7,7 @@ password=abcd1234
 echo Print the value of A is $aRupess ####### it doesnot work because for $a and Rupees need space
 echo Print the value of A is $a Rupess  ########## it works like "10 Rupees"
 echo Print the value of A is ${a}Rupees ########### it also works like "10Rupees"
+
+
+component=catalogue
+echo here the Component is ${component}
