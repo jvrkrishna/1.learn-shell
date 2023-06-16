51314fee0829a24c9a60b9a01e9137e05d31f432
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 22:32:34 +0530

    terraform

---
+++ 08.conditions.sh
@@ -0,0 +1,23 @@
+############## simple if #############
+#if [ expression ]; then
+# 	Commands
+#fi
+
+########## if else ################
+#if [ expression ]; then
+#    Commands
+#else
+#    commands
+#fi
+
+############## else if ##################
+#if [ expression1 ]; then
+#    Commands
+#Elif [ expression2 ];
+#then
+#    commands
+#else
+#    commands
+#fi
+
+
