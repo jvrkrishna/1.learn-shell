6ef15d3fd1e93ee9078839616d18213aa47d8a5b
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 21:58:49 +0530

    terraform

--- 05.variables.sh
+++ 05.variables.sh
@@ -5,5 +5,5 @@ password=abcd1234
 
 
 echo Print the value of A is $aRupess ####### it doesnot work because for $a and Rupees need space
-echo Print the value of A is $aRupess  ########## it works
+echo Print the value of A is $a Rupess  ########## it works
 echo Print the value of A is ${a}Rupees ########### it also works
