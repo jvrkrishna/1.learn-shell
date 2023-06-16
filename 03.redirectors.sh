adbb143023866feedba5228a8b13c394e56796a3
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-16 21:04:05 +0530

    terraform

---
+++ 03.redirectors.sh
@@ -0,0 +1,8 @@
+#     > overwrite the previous content
+#    >>  Append to the previous content
+
+#    1> Redirects the output
+#    2> Redirects the error
+#    &>> Redirects both output and error
+
+yum install nginx -y &>>/tmp/sampl.log
